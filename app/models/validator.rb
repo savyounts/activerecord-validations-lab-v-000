f6e60ea8a0d217df4e261192bce0dacf8d67b3f8
Savannah Younts <savyounts@gmail.com>
2018-09-20 21:17:38 +0000

    automatically backed up by learn

--- app/models/validator.rb
+++ app/models/validator.rb
@@ -1,8 +1,8 @@
 class Validator < ActiveModel::Validator
 
-  def clickbaity
-    unless title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
-      errors.add(:title, "isn't cool enough")
+  def clickbaity(post)
+    unless post.title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+      post.errors.add(:title, "isn't cool enough")
     end
   end
 end
