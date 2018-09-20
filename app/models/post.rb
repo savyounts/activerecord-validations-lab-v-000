4bafbb966357e9179fe77a0484531a65f1250d6f
Savannah Younts <savyounts@gmail.com>
2018-09-20 21:08:56 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
-  validates :title, :clickbaity
+  validates :clickbaity
 
   def clickbaity(post)
     unless post.title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
