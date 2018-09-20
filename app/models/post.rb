eb4eccdbad5d59daef303963955da9db21bd5e9c
Savannah Younts <savyounts@gmail.com>
2018-09-20 21:26:48 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with Validator
+  # include ActiveModel::Validations
+  # validates_with Validator
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates_inclusion_of :title, in:%w(Won't Believe Secret Top [number] Guess)
 
 
 
