96809b6a6c9cc90697ae2e68809f10c83cf8c520
Savannah Younts <savyounts@gmail.com>
2018-09-20 21:49:38 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,14 +7,16 @@ class Post < ActiveRecord::Base
 
   validates_with Validator
 
-  class Validator < ActiveModel::Validator
+  
 
-    def validate(post)
-      unless post.title.include?("Won't Believe") || post.title.include?("Secret") || post.title.include?("Guess")
-        post.errors.add(:title, "isn't cool enough")
-      end
+
+end
+
+class Validator < ActiveModel::Validator
+
+  def validate(post)
+    unless post.title.include?("Won't Believe") || post.title.include?("Secret") || post.title.include?("Guess")
+      post.errors.add(:title, "isn't cool enough")
     end
   end
-
-
 end
