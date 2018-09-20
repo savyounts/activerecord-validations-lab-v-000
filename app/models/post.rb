dec6a7a0215d806d702f8a22866f6abd27409fb6
Savannah Younts <savyounts@gmail.com>
2018-09-20 21:44:58 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
-  
+
   validates_with Validator
 
   class Validator < ActiveModel::Validator
