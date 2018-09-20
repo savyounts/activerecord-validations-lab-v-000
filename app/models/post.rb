9276eae571ebb4ecdbdce97af954d2c2f2f2e1f9
Savannah Younts <savyounts@gmail.com>
2018-09-20 21:44:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,20 @@
 class Post < ActiveRecord::Base
-
+  include ActiveModel::Validations
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
-  include ActiveModel::Validations
+  
   validates_with Validator
+
+  class Validator < ActiveModel::Validator
+
+    def validate(post)
+      unless post.title.include?("Won't Believe") || post.title.include?("Secret") || post.title.include?("Guess")
+        post.errors.add(:title, "isn't cool enough")
+      end
+    end
+  end
+
 
 end
